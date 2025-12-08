379ebb16dbb55900b6f4398fac4bd426c8221f1c
Davi Francatto <davi.francatto2@gmail.com>
2025-12-08 10:01:13 -0300

    Adicionado Aula 3 - Strings

---
+++ aula3.py
@@ -0,0 +1,27 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+
+print('Davi Francatto')
+print(1, 'Davi "Francatto"')
+
+
+# Aspas duplas
+
+print("Davi Francatto")
+print(2, "Davi 'Francatto'")
+
+# Escape
+
+print("Davi \"Francatto\"")
+
+# r
+
+print(r"Davi \"Francatto\"")
