bfb97104afb3fea8713f459878c5a9355e211c00
Davi Francatto <davi.francatto2@gmail.com>
2025-12-08 09:45:58 -0300

    git commit -m Adicionado Aula 2 - Linhas e Quebras de Linhas"

---
+++ aula1.py
@@ -0,0 +1,12 @@
+"""
+Escrever oque eu
+quiser em varias linhas (DocString)
+"""
+'''
+Usar para escrever suas notas
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
