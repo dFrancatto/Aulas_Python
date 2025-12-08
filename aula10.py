40d344323072d7ff977d694170f5401b88827ef5
Davi Francatto <davi.francatto2@gmail.com>
2025-12-08 14:08:31 -0300

    Adicionado Aula 10 - Concatenação e Repetição

---
+++ aula10.py
@@ -0,0 +1,12 @@
+# Concatenação
+nome= 'Davi' + ' ' + 'Francatto'
+print(nome)
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+# Repetição
+a_dez_vezes = 'A' * 10
+tres_vezes_davi = 3 * 'Davi'
+print(a_dez_vezes)
+print(tres_vezes_davi)
+
