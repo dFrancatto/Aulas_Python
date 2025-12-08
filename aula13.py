c88c576a6c94b0cef39c85dc6e8f2e97494b1148
Davi Francatto <davi.francatto2@gmail.com>
2025-12-08 15:55:59 -0300

    Adicionado Aula 13 - Formatação de Strings

---
+++ aula13.py
@@ -0,0 +1,10 @@
+
+nome = "Davi"
+peso = 118.0
+altura = 1.78
+imc = peso // (altura **2)
+
+Linha1_ = f'{nome} tem {altura:.2f} de altura'
+
+print(nome,"tem",altura, "de altura,","pesa", peso,"KG", "e seu IMC é:")
+print(imc)
