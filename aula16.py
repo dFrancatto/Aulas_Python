640b201e818619491ba122abc352c3195283919c
Davi Francatto <davi.francatto2@gmail.com>
2026-01-21 09:08:16 -0300

    Adicionado Aula 17 - Exercicio IF

--- aula16.py
+++ aula16.py
@@ -1,10 +1,13 @@
 entrada = input('Você quer "entrar" ou "sair"? ')
 
 
-if entrada == 'entrar': print('Você entrou no sistema')
+if entrada == 'entrar': 
+    print('Você entrou no sistema')
 
-elif entrada == 'sair': print('Você saiu do sistema')
+elif entrada == 'sair': 
+    print('Você saiu do sistema')
 
-else:   print('Você não digitou nada')
+else:   
+    print('Você não digitou nada')
 
 print('FORA DOS BLOCOS')
---
+++ aula17.py
@@ -0,0 +1,25 @@
+condicao = True
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Codigo para condição 1')
+
+elif condicao2:
+    print('Codigo para condição 2')
+
+elif condicao3:
+    print('Codigo para condição 3')
+
+elif condicao4:
+    print('Codigo para condição 4')  
+
+else: 
+    print('Nenhuma condição foi satisfeita')
+
+if 10==10: 
+    print('Outro IF')
+
+print('Fora do if')
